e6f3a687bf7e7c3594834d015b8ef6936c09fb46
Marc Wright <marc@generalassemb.ly>
2019-04-16 09:39:50 +0300

    starting mashable

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var diggFeed = [], diggNodes = [];
 var redditArticles= [], redditNodes = [];
+var mashableArticles =[], mashableNodes = [];
 var $loader = $('.loader');
 var $popUp = $('#popUp');
 var $main = $('#main');
@@ -103,6 +104,10 @@ function setDropDown() {
     if (e.target.id === "reddit") { 
       // $main.empty()
       redditAppendDom() 
+    } else if (e.target.id === "digg") {
+      getDiggArticles()
+    } else {
+      alert("Mashable not working yet")
     }
   })
 }
@@ -133,16 +138,35 @@ function redditAppendDom() {
   $main.empty()
   $loader.removeClass('hidden');
   appendDom(redditArticles)
-  $loader.addClass('hidden');
-
-  
+  $loader.addClass('hidden');  
 }
 
+function getMashableArticles() {
+  $.get('https://www.reddit.com/top.json')
+    .done(response => {
+      // redditArticles = response.data.children
+      // console.log(redditArticles)
+      response.data.children.forEach(article =>  {
+        // console.log(article)
+        redditArticles.push({
+          title: article.data.title,
+          url: article.data.url,
+          image: article.data.thumbnail,
+          source: article.data.subreddit,
+          description: article.data.subreddit
+        })
+      })
+    })
+    .done(() => {
+      console.log(redditArticles)
+    })
+}
 
 $(() => {
   $loader.removeClass('hidden');
   getDiggArticles()
   getRedditArticles()
+  getMashableArticles()
   setDropDown()
 })
 
