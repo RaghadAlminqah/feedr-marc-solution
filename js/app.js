54351ff11932953e1f86f34503c4ae398ba44b61
Marc Wright <marc@generalassemb.ly>
2019-04-16 11:03:25 +0300

    refactor into async await and starting into one append dom

--- js/app.js
+++ js/app.js
@@ -66,52 +66,48 @@ function setDropDown() {
   })
 }
 
-function getAllArticles() {
-  $.get('https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json')
-    .done(response => {
-      response.data.feed.forEach(article =>  {
-        diggArticles.push({
-          title: article.content.title,
-          url: article.content.url,
-          image: article.content.media.images[0].original_url,
-          source: article.content.domain_name,
-          description: article.content.description
-        })
-      })
-      diggAppendDom()
+async function getAllArticles() {
+  let diggResponse = await $.get('https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json')
+  let redditResponse = await $.get('https://www.reddit.com/top.json')
+  let mashableResponse = await $.get('https://accesscontrolalloworiginall.herokuapp.com/https://mashable.com/stories.json')
+
+  await buildNodes(diggResponse, redditResponse, mashableResponse)
+  console.log("Articles loaded");
+  $popUp.addClass('hidden')
+  return diggAppendDom()
+}
+  
+
+function buildArticles(diggResponse, redditResponse, mashableResponse) {
+  diggResponse.data.feed.forEach(article =>  {
+    diggArticles.push({
+      title: article.content.title,
+      url: article.content.url,
+      image: article.content.media.images[0].original_url,
+      source: article.content.domain_name,
+      description: article.content.description
     })
-    .done(() => {
-      $.get('https://www.reddit.com/top.json')
-        .done(response => {
-          response.data.children.forEach(article =>  {
-            redditArticles.push({
-              title: article.data.title,
-              url: article.data.url,
-              image: article.data.thumbnail,
-              source: article.data.subreddit,
-              description: article.data.subreddit
-            })
-          })
-        })
+  })
+
+  redditResponse.data.children.forEach(article =>  {
+    redditArticles.push({
+      title: article.data.title,
+      url: article.data.url,
+      image: article.data.thumbnail,
+      source: article.data.subreddit,
+      description: article.data.subreddit
     })
-    .done(() => {
-      $.get('https://accesscontrolalloworiginall.herokuapp.com/https://mashable.com/stories.json')
-        .done(response => {
-          response.hot.forEach(article =>  {
-            mashableArticles.push({
-              title: article.display_title,
-              url: article.link,
-              image: article.image,
-              source: article.channel,
-              description: article.excerpt
-            })
-          })
-        })
-      })
-    .done(() => {
-      console.log("Articles loaded");
-      $popUp.addClass('hidden')
+  })
+
+  mashableResponse.hot.forEach(article =>  {
+    mashableArticles.push({
+      title: article.display_title,
+      url: article.link,
+      image: article.image,
+      source: article.channel,
+      description: article.excerpt
     })
+  })
 }  
 
 function diggAppendDom() {
